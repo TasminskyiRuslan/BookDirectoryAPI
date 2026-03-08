cb3b08ef948ab8f2d51b54650466c6f2f15d7a60
TasminskyiRuslan <tasminskyi.ruslan@gmail.com>
2026-03-08 18:02:31 +0000

    Changed User model, factory and seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * This seeder populates the users table.
+     */
+    public function run(): void
+    {
+        User::factory()->count(7)->create();
+    }
+}
